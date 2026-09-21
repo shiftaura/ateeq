fab0efa0cd8198dbe2e63fb6304c29155a69a28d
shiftaura <shiftaura@zohomail.in>
2026-09-21 21:44:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [env.CLIENT_URL, "http://localhost:5173", "https://ateeq-amber.vercel.app"],
+    origin: [process.env.CLIENT_URL, "http://localhost:5173", "https://ateeq-amber.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
